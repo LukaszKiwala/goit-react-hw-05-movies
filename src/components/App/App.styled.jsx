02fbb5cf6c5ec6adc4e6154bdcc5b84854b89890
LukaszKiwala <113800025+LukaszKiwala@users.noreply.github.com>
2023-07-22 13:46:24 +0200

    debbuging errors

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,1 +1,9 @@
-image.png
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  max-width: 1600px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
+`;
